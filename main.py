18acd03d92708d993441931c1dfe4e7de31e2c1e
Азат Сиразетдинов <azat222@gmail.com>
2021-12-14 22:50:04 +0500

    make it better

--- main.py
+++ main.py
@@ -11,36 +11,50 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 wait_seconds = 1800
 last_time_banned = datetime.datetime.now() - datetime.timedelta(seconds=wait_seconds)
+massive = [1 for i in users]
 
 
 # -1001458827756
 def form_repr(num):
+    global massive
+    pistol = emojize(":water_pistol:")
     out = ''
     if num == -1:
         for person in users:
-            out += f'— {person.name}{"" if person.surname is None else " " + str(person.surname)},\n'
+            out += f'{pistol}{person.name}{"" if person.surname is None else " " + str(person.surname)},\n'
     else:
         for per_num, person in enumerate(users):
-            if per_num != num:
-                out += f'— {person.name}{"" if person.surname is None else " " + str(person.surname)},\n'
+            if massive[per_num] == 1:
+                out += f'{pistol}{person.name}{"" if person.surname is None else " " + str(person.surname)},\n'
             else:
-                out += f'{emojize(":water_pistol:")}{person.name}{"" if person.surname is None else " " + str(person.surname)},\n'
+                out += f'— {person.name}{"" if person.surname is None else " " + str(person.surname)},\n'
     return out
+
 
 
 @dp.message_handler(commands=['ban'])
 async def kill_sbd(message: types.Message):
-    global last_time_banned
+    global last_time_banned, massive
     now_time = datetime.datetime.now()
     delta = now_time - last_time_banned
     if delta.seconds > wait_seconds or delta.days > 0:
+        massive = [1 for i in users]
         last_time_banned = now_time
-        last_message = await message.reply(form_repr(10))
-        num_to_ban = randint(0, len(users) - 1)
-        for i in range(0, len(users) + num_to_ban + 1):
-            sleep(i / 90)
-            await bot.edit_message_text(form_repr(i % len(users)), message.chat.id, last_message.message_id)
-        sleep(1)
+        last_message = await message.reply(form_repr(-1))
+        # num_to_ban = randint(0, len(users) - 1)
+        num_to_ban = 0
+        while sum(massive) != 1:
+            sleep(sum(massive) / 100)
+            num1 = randint(0, len(users) - 1)
+            while num1 == num_to_ban or massive[num1] == 0:
+                num1 = randint(0, len(users) - 1)
+            num2 = randint(0, len(users) - 1)
+            while num2 == num_to_ban or massive[num2] == 0:
+                num2 = randint(0, len(users) - 1)
+            massive[num1] = 0
+            massive[num2] = 0
+            await bot.edit_message_text(form_repr(num_to_ban), message.chat.id, last_message.message_id)
+        sleep(4)
         await bot.edit_message_text(
             f'{users[num_to_ban].name} {"" if users[num_to_ban].surname is None else " " + str(users[num_to_ban].surname) + " "}забанен на час{emojize(":smiling_face_with_horns:")}',
             message.chat.id, last_message.message_id)
